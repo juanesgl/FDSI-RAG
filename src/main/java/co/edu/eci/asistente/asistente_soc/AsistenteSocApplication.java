2836349f0c7230e11f32cd7de73fa0a331618bcc
juanesgl <juan.sgarcia@mail.escuelaing.edu.co>
2026-09-13 13:54:24 -0500

    feature: Se añade la config basica de carpetas, tambien mvnw en caso para CI/CD

---
+++ src/main/java/co/edu/eci/asistente/asistente_soc/AsistenteSocApplication.java
@@ -0,0 +1,13 @@
+package co.edu.eci.asistente.asistente_soc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenteSocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenteSocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/eci/asistente/asistente_soc/AsistenteSocApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eci.asistente.asistente_soc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenteSocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
